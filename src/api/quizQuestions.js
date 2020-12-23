c462d87a4cb34a597d760be160df3c69adc56eff
mowr02 <hello@lizmowforth.com>
2020-12-24 00:07:35 +0100

    updating styling

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+  {
+      question: "How many drinks have you had?",
+      answers: [
+          {
+              type: "this Nicholas Cage scatter cushion",
+              content: "10+"
+          },
+          {
+              type: "4 slightly terrified hamsters",
+              content: "4+"
+          },
+          {
+              type: "nothing. You deserve nothing.",
+              content: "0"
+          }
+      ]
+  },
+  {
+      question: "What were you drinking?",
+      answers: [
+          {
+              type: "this Nicholas Cage scatter cushion",
+              content: "Beer"
+          },
+          {
+              type: "4 slightly terrified hamsters",
+              content: "Wine"
+          },
+          {
+              type: "nothing. You deserve nothing.",
+              content: "Water"
+          }
+      ]
+  },
+  {
+      question: "Do you have a penis or a vagina?",
+      answers: [
+          {
+              type: "this Nicholas Cage scatter cushion",
+              content: "Pee-Pee"
+          },
+          {
+              type: "4 slightly terrified hamsters",
+              content: "Vagine"
+          },
+          {
+              type: "nothing. You deserve nothing.",
+              content: "No"
+          }
+      ]
+  },
+  {
+      question: "Are you alone?",
+      answers: [
+          {
+              type: "this Nicholas Cage scatter cushion",
+              content: "Yes"
+          },
+          {
+              type: "4 slightly terrified hamsters",
+              content: "No"
+          },
+          {
+              type: "nothing. You deserve nothing.",
+              content: "I have cats"
+          }
+      ]
+  },
+  {
+      question: "Are you Jeff Goldblum?",
+      answers: [
+          {
+              type: "this Nicholas Cage scatter cushion",
+              content: "Uh-huh"
+          },
+          {
+              type: "4 slightly terrified hamsters",
+              content: "Not really"
+          },
+          {
+              type: "nothing. You deserve nothing.",
+              content: "Haven't yet considered it."
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
